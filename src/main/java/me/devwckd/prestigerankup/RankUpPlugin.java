a8750a87bc5d05f82a3d254fa14784bcfba1f57f
devwckd <dev.wckd@gmail.com>
2020-06-09 16:31:48 -0300

    First Commit 🍕

---
+++ src/main/java/me/devwckd/prestigerankup/RankUpPlugin.java
@@ -0,0 +1,11 @@
+package me.devwckd.prestigerankup;
+
+import co.wckd.boilerplate.plugin.BoilerplatePlugin;
+
+public class RankUpPlugin extends BoilerplatePlugin {
+
+    @Override
+    public void enable() {
+
+    }
+}
