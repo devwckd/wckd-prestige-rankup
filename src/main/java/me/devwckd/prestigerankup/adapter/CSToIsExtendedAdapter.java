f766896c760cb223e55428561097e916dc26032e
devwckd <dev.wckd@gmail.com>
2020-06-09 19:32:51 -0300

    Extended itemstack adapter to accept base64 and owner based skulls.

---
+++ src/main/java/me/devwckd/prestigerankup/adapter/CSToIsExtendedAdapter.java
@@ -0,0 +1,38 @@
+package me.devwckd.prestigerankup.adapter;
+
+import co.wckd.boilerplate.adapter.CSToISAdapter;
+import me.devwckd.prestigerankup.util.HeadUtils;
+import org.bukkit.Material;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class CSToIsExtendedAdapter extends CSToISAdapter {
+
+    @Override
+    public ItemStack adapt(MemorySection from) {
+        ItemStack adapted = super.adapt(from);
+        if(adapted == null) return null;
+
+        if(adapted.getType() != Material.SKULL_ITEM || adapted.getDurability() != 3) return adapted;
+
+        String owner = from.getString("owner");
+        if(owner != null) {
+            SkullMeta skullMeta = ((SkullMeta) adapted.getItemMeta());
+            skullMeta.setOwner(owner);
+            adapted.setItemMeta(skullMeta);
+            return adapted;
+        }
+
+        String skin = from.getString("skin");
+        if(skin != null) {
+            return HeadUtils.injectSkin(adapted, skin);
+        }
+
+        return adapted;
+
+    }
+}
---
+++ src/main/java/me/devwckd/prestigerankup/util/HeadUtils.java
@@ -0,0 +1,55 @@
+package me.devwckd.prestigerankup.util;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+import static me.devwckd.prestigerankup.util.ReflectionsUtils.*;
+
+public class HeadUtils {
+
+    private static final Class GAME_PROFILE_CLASS = getReflectionClass("com.mojang.authlib.GameProfile");
+    private static final Class PROPERTY_MAP_CLASS = getReflectionClass("com.mojang.authlib.properties.PropertyMap");
+    private static final Class PROPERTY_CLASS = getReflectionClass("com.mojang.authlib.properties.Property");
+    private static final Constructor GAME_PROFILE_CONSTRUCTOR = getReflectionConstructor(GAME_PROFILE_CLASS, UUID.class, String.class);
+    private static final Constructor PROPERTY_CONSTRUCTOR = getReflectionConstructor(PROPERTY_CLASS, String.class, String.class);
+    private static final Method GET_PROPERTIES_METHOD = getReflectionDeclaredMethod(GAME_PROFILE_CLASS, "getProperties");
+    private static final Method REMOVE_ALL_METHOD = getReflectionDeclaredMethod(PROPERTY_MAP_CLASS, "removeAll", String.class);
+    private static final Method PUT_METHOD = getReflectionDeclaredMethod(PROPERTY_MAP_CLASS, "put", PROPERTY_CLASS);
+    private static final Field PROFILE_FIELD = getReflectionDeclaredField(SkullMeta.class, "profile");
+
+    public static ItemStack injectSkin(ItemStack stack, String skin) {
+
+        if(GAME_PROFILE_CONSTRUCTOR == null) throw new NullPointerException("gameProfileConstructor cannot be null!");
+        if(GET_PROPERTIES_METHOD == null) throw new NullPointerException("getPropertiesMethod cannot be null!");
+        if(REMOVE_ALL_METHOD == null) throw new NullPointerException("removeAllMethod cannot be null!");
+        if(PROPERTY_CONSTRUCTOR == null) throw new NullPointerException("propertyConstructor cannot be null!");
+        if(PUT_METHOD == null) throw new NullPointerException("putMethod cannot be null!");
+        if(PROFILE_FIELD == null) throw new NullPointerException("profileField cannot be null!");
+
+        Object gameProfileObject = newReflectionInstance(GAME_PROFILE_CONSTRUCTOR, UUID.randomUUID(), null);
+        if(gameProfileObject == null) throw new NullPointerException("gameProfileObject cannot be null!");
+
+        Object propertyMapObject = invokeReflectionMethod(GET_PROPERTIES_METHOD, gameProfileObject);
+        if(propertyMapObject == null) throw new NullPointerException("propertyMapObject cannot be null!");
+        invokeReflectionMethod(REMOVE_ALL_METHOD, propertyMapObject, "textures");
+
+        Object propertyObject = newReflectionInstance(PROPERTY_CONSTRUCTOR, "textures", skin);
+        if(propertyObject == null) throw new NullPointerException("propertyObject cannot be null!");
+        invokeReflectionMethod(PUT_METHOD, propertyObject, propertyObject);
+
+        SkullMeta skullMeta = ((SkullMeta) stack.getItemMeta());
+        setReflectionFieldAccessible(PROFILE_FIELD, true);
+        setReflectionField(PROFILE_FIELD, skullMeta, gameProfileObject);
+        setReflectionFieldAccessible(PROFILE_FIELD, false);
+        stack.setItemMeta(skullMeta);
+
+        return stack;
+
+    }
+
+}
