ee82470ba6ab4c8e6d9e925f9d1b8fa912c983bc
devwckd <dev.wckd@gmail.com>
2020-06-09 18:38:27 -0300

    Instantiated adapter.

--- src/main/java/me/devwckd/prestigerankup/RankUpPlugin.java
+++ src/main/java/me/devwckd/prestigerankup/RankUpPlugin.java
@@ -1,16 +1,29 @@
 package me.devwckd.prestigerankup;
 
+import co.wckd.boilerplate.adapter.Adapter;
+import co.wckd.boilerplate.adapter.AdapterImpl;
+import co.wckd.boilerplate.adapter.CSToISAdapter;
 import co.wckd.boilerplate.plugin.BoilerplatePlugin;
 import lombok.Getter;
 import me.devwckd.prestigerankup.lifecycle.FileLifecycle;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.inventory.ItemStack;
 
 @Getter
 public class RankUpPlugin extends BoilerplatePlugin {
 
     private final FileLifecycle fileLifecycle = lifecycle(new FileLifecycle(this), 0);
 
+    private Adapter adapter;
+
+    @Override
+    public void load() {
+        adapter = new AdapterImpl();
+        adapter.registerAdapter(MemorySection.class, ItemStack.class, new CSToISAdapter());
+    }
+
     @Override
     public void enable() {
-
     }
+
 }
