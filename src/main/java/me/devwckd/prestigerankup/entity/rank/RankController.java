402ca6a7d3f06191c72ad07272976ece8366ad7e
devwckd <dev.wckd@gmail.com>
2020-06-10 15:32:28 -0300

    Finished(?) rank controller

--- src/main/java/me/devwckd/prestigerankup/entity/rank/RankController.java
+++ src/main/java/me/devwckd/prestigerankup/entity/rank/RankController.java
@@ -16,20 +16,28 @@ public class RankController {
         ranks.addIndex(onAttribute(Rank.ID));
     }
 
-    public void insert(Rank rank) {
-        ranks.add(rank);
-    }
-
-    public boolean isEmpty() {
-        return ranks.isEmpty();
-    }
-
     public Rank getByPosition(int position) {
         try {
             return ranks.retrieve(equal(Rank.POSITION, position)).uniqueResult();
         } catch (Exception $) {
             return null;
         }
+    }
+
+    public Rank getByID(String id) {
+        try {
+            return ranks.retrieve(equal(Rank.ID, id.toLowerCase())).uniqueResult();
+        } catch (Exception $) {
+            return null;
+        }
+    }
+
+    public void insert(Rank rank) {
+        ranks.add(rank);
+    }
+
+    public boolean isEmpty() {
+        return ranks.isEmpty();
     }
 
 }
