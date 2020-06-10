f85a5ea92c3875ca2da158e8a4f6bb17a11c7e62
devwckd <dev.wckd@gmail.com>
2020-06-10 16:51:28 -0300

    Made user controller's base

---
+++ src/main/java/me/devwckd/prestigerankup/entity/user/UserController.java
@@ -0,0 +1,37 @@
+package me.devwckd.prestigerankup.entity.user;
+
+import com.googlecode.cqengine.ConcurrentIndexedCollection;
+import com.googlecode.cqengine.IndexedCollection;
+
+import java.util.UUID;
+
+import static com.googlecode.cqengine.index.navigable.NavigableIndex.*;
+import static com.googlecode.cqengine.query.QueryFactory.*;
+
+public class UserController {
+
+    private final IndexedCollection<User> users;
+
+    public UserController() {
+        users = new ConcurrentIndexedCollection<>();
+        users.addIndex(onAttribute(User.UUID));
+        users.addIndex(onAttribute(User.LOWERCASE_NICKNAME));
+    }
+
+    public User getByUUID(UUID uuid) {
+        try {
+            return users.retrieve(equal(User.UUID, uuid)).uniqueResult();
+        } catch (Exception $) {
+            return null;
+        }
+    }
+
+    public User getByLowercaseNickname(String lowercaseNickname) {
+        try {
+            return users.retrieve(equal(User.LOWERCASE_NICKNAME, lowercaseNickname)).uniqueResult();
+        } catch (Exception $) {
+            return null;
+        }
+    }
+    
+}
